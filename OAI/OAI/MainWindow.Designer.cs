b7f648ccc1441d35d0163e75a9c60cb6082188c1
campcoral <kl.kiruxa2006@gmail.com>
2024-08-13 20:32:47 +0300

    Add files via upload

---
+++ OAI/OAI/MainWindow.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace OAI
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ADB status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(73, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Disconnected";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(313, 21);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Install";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label3.Location = new System.Drawing.Point(12, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(313, 176);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Drag and Drop\r\nor\r\nPick apk file";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 247);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "Old APK Installer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        #endregion
+    }
+}
---
+++ OAI/OAI/MainWindow.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OAI
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            string pickedFilePath = null;
+
+            FormClosing += (sender, e) =>
+            {
+                KillADBServer();
+            };
+
+            label3.DragDrop += (sender, e) =>
+            {
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                label3.Text = files[0].ToString();
+                pickedFilePath = files[0].ToString();
+            };
+
+            label3.DragEnter += (sender, e) =>
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+            };
+            
+            label3.Click += (sender, e) => {
+                 OpenFileDialog ofd = new OpenFileDialog();
+                 ofd.Filter = "APK files (*.apk)|*.apk";
+            
+                 if(ofd.ShowDialog() == DialogResult.OK) {
+                     label3.Text = ofd.FileName;
+                     pickedFilePath = ofd.FileName;
+                 }
+            };
+            button2.Click += (sender, e) =>
+            {
+                InstallApp(button2, pickedFilePath);
+            };
+
+            var thread = new Thread(async () =>
+            {
+                while (true)
+                {
+                    if (CheckADBConnection())
+                    {
+                        label2.Text = "Connected";
+                        label2.ForeColor = Color.Green;
+                        button2.Enabled = true;
+                    }
+                    else
+                    {
+                        label2.Text = "Disconnected";
+                        label2.ForeColor = Color.Red;
+                        button2.Enabled = false;
+                    }
+                    await Task.Delay(500);   
+                }
+            });
+            thread.Start();
+        }
+
+        private static void KillADBServer()
+        {
+            string adbCommand = $"adb kill-server";
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {adbCommand}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+        }
+        private static void InstallApp(Button button, string path)
+        {
+            string adbCommand = $"adb install --bypass-low-target-sdk-block {path}";
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {adbCommand}",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            using (Process process = new Process())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+                button.Text = "Wait...";
+                
+                process.WaitForExit();
+                button.Text = "Install";
+            }
+        }
+        private static bool CheckADBConnection()
+        {
+            try
+            {
+                string adbCommand = "adb devices";
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    Arguments = $"/c {adbCommand}",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                using (Process process = new Process())
+                {
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    StringReader st = new StringReader(output);
+                    st.ReadLine();
+                    var x = st.ReadLine();
+                    return x.Contains("device");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                return false;
+            }
+        }
+    }
+}
